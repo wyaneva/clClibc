3209be72f1da46a8bcb42a8789e670c4ff395352
Vanya Yaneva <wyaneva@gmail.com>
2020-06-04 16:21:13 +0100

    Moved `cl-atoi` to `cl-stdlib`

--- cl-stdlib.h
+++ cl-stdlib.h
@@ -1,9 +1,9 @@
 /*
- * cl-atoi.h
- * String to int conversion 
+ * cl-stdlib.h
+ * String to int conversion
  *
- * Copyright 2016 Vanya Yaneva, The University of Edinburgh
- *   
+ * Copyright 2016-2020 Vanya Yaneva, The University of Edinburgh
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -17,75 +17,69 @@
  * limitations under the License.
  */
 
-#ifndef CL_ATOI_H
-#define CL_ATOI_H
+#ifndef CL_STDLIB_H
+#define CL_STDLIB_H
 
 #include "cl-ctype.h"
 
-//integer
+// integer
 int atoi(const char *);
 
-//long versions
+// long versions
 long int atol(const char *);
 
-int atoi(const char *str)
-{
-    int res = 0;
-    int sign = 1;
+int atoi(const char *str) {
+  int res = 0;
+  int sign = 1;
 
-    // Skip any initial empty spaces
-    while (*str == ' ')
-        str++;
-  
-    // If number is negative, then update sign
-    if (*str == '-')
-    {
-        sign = -1;
-        str++;
-    }
-  
-    // Iterate through all digits of input string and update result
-    while (*str != '\0')
-    {
-        if (!isdigit(*str))
-          break;
+  // Skip any initial empty spaces
+  while (*str == ' ')
+    str++;
 
-        res = res*10 + *str - '0';
-        str++;
-    }
-  
-    // Return result with sign
-    return sign*res;
+  // If number is negative, then update sign
+  if (*str == '-') {
+    sign = -1;
+    str++;
+  }
+
+  // Iterate through all digits of input string and update result
+  while (*str != '\0') {
+    if (!isdigit(*str))
+      break;
+
+    res = res * 10 + *str - '0';
+    str++;
+  }
+
+  // Return result with sign
+  return sign * res;
 }
 
-long int atol(const char *str)
-{
+long int atol(const char *str) {
   long int res = 0;
   int sign = 1;
 
   // Skip any initial empty spaces
   while (*str == ' ')
     str++;
-  
+
   // If number is negative, then update sign
-  if (*str == '-')
-  {
+  if (*str == '-') {
     sign = -1;
     str++;
   }
-  
+
   // Iterate through all digits of input string and update result
-  while (*str != '\0')
-  {
+  while (*str != '\0') {
     if (!isdigit(*str))
       break;
 
-     res = res*10 + *str - '0';
-     str++;
+    res = res * 10 + *str - '0';
+    str++;
   }
-  
+
   // Return result with sign
-  return sign*res;
+  return sign * res;
 }
 
 #endif
